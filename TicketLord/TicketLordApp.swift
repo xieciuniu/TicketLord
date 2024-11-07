c842c77e3aed46a08a88ab7bd46849fac152a01b
Hubert Wojtowicz <h.wojtowicz44@gmail.com>
2024-11-07 09:18:16 +0100

    initial commit

---
+++ TicketLord/TicketLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketLordApp.swift
+//  TicketLord
+//
+//  Created by Hubert Wojtowicz on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicketLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicketLordTests/TicketLordTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicketLordTests.swift
+//  TicketLordTests
+//
+//  Created by Hubert Wojtowicz on 07/11/2024.
+//
+
+import Testing
+@testable import TicketLord
+
+struct TicketLordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
